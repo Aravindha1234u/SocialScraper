af2f75e5609c7e563b949e9e5bbf5fb0af4a1b56
aravindha1234u <aravindha1234u@gmail.com>
2020-02-25 20:59:10 +0530

    AI Image Recognition

--- social/api.py
+++ social/api.py
@@ -1,2 +1,6 @@
 def google():
     return "AIzaSyAiZKZpjH2w_Ku3qUKIMsNf2UgD2Q3mCes&cx=005553992128112222999:w1eonxpepby"
+def api_key():
+    return "acc_7a12e1ad1c7a241"
+def api_secret():
+    return '848fffd1730d47352bbdfd8c52b4aae6'
--- social/image.py
+++ social/image.py
@@ -1,12 +1,13 @@
 import requests
+from api import api_key,api_secret
 
 R = '\033[31m' # red
 G = '\033[32m' # green
 C = '\033[36m' # cyan
 W = '\033[0m'  # white
 
-def imageai(url):
-    '''
+def nsfw(url):
+    #deepai api
     r = requests.post(
         "https://api.deepai.org/api/nsfw-detector",
         files={
@@ -14,12 +15,13 @@ def imageai(url):
         },
         headers={'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'}
     )
-    data=r.json()'''
-    data={'output': {'detections': [{'confidence': '0.97', 'bounding_box': [186, 629, 309, 304], 'name': 'Female Breast - Exposed'}, {'confidence': '0.97', 'bounding_box': [461, 442, 256, 258], 'name': 'Female Breast - Exposed'}], 'nsfw_score': 0.9958459734916687}, 'id': '9ead193a-067b-4f83-8f6a-9fe5ee1ff05d'}
+    data=r.json()
+
     for i in data['output']['detections']:
         for j in i.keys():
             if str(j)=="confidence" and float(i[j])*100 > 75:
                 print(R+str(j)+" : "+str(float(i[j])*100)+"%")
+                nsfw(url)
             elif str(j)=="confidence" and float(i[j])*100 > 50:
                 print(C+str(j)+" : "+str(float(i[j])*100)+"%")
             elif str(j)=="confidence" and float(i[j])*100 > 35:
@@ -27,3 +29,27 @@ def imageai(url):
             else:
                 print(W+str(j)+" : "+str(i[j]))
         print()
+def imageai(url):
+    #imagga api
+    key = api_key()
+    secret = api_secret()
+
+    r = requests.post(
+        'https://api.imagga.com/v2/tags',
+        auth=(key, secret),
+        files={'image': open(url, 'rb')})
+    data=r.json()
+    #exit()
+    for i in data['result']['tags']:
+        for j in i.keys():
+            if str(j)=="confidence" and float(i[j]) > 75:
+                print(R+str(j)+" : "+str(float(i[j]))+"%")
+                #nsfw(url)
+            elif str(j)=="confidence" and float(i[j]) > 50:
+                print(C+str(j)+" : "+str(float(i[j]))+"%")
+            elif str(j)=="confidence" and float(i[j]) > 35:
+                print(G+str(j)+" : "+str(float(i[j]))+"%")
+            else:
+                print(W+str(j)+" : "+str(i[j]))
+        print()
+#imageai("/root/Downloads/SocialScraper/image.jpeg")
