50787d3215bebbc3b334d15ef330384ebbcac4d0
aravindha1234u <aravindha1234u@gmail.com>
2020-03-03 12:41:58 +0530

    Updated

--- social/image.py
+++ social/image.py
@@ -11,7 +11,7 @@ W = '\033[0m'  # white
 
 def nsfw(url):
     #deepai api
-    '''
+
     r = requests.post(
         "https://api.deepai.org/api/nsfw-detector",
         files={
@@ -22,8 +22,8 @@ def nsfw(url):
     data=r.json()
 
     '''
-    data={'result':{'tags':[{'confidence' : '70.77', 'tag' : {'en': 'bikini'}},{'confidence' : '7.08091878890991','tag' : {'en': 'summer'}}]}}
-
+    data={'output':{'detections':{'result':{'tags':[{'confidence' : '70.77', 'tag' : {'en': 'bikini'}},{'confidence' : '7.08091878890991','tag' : {'en': 'summer'}}]}}}}
+    '''
     confidence=[]
     for i in data['output']['detections']:
         for j in i.keys():
@@ -46,7 +46,7 @@ def imageai(url):
     #imagga api
     key = api_key()
     secret = api_secret()
-    '''
+
     r = requests.post(
         'https://api.imagga.com/v2/tags',
         auth=(key, secret),
@@ -55,7 +55,7 @@ def imageai(url):
 
     '''
     data={'result':{'tags':[{'confidence' : '70.77', 'tag' : {'en': 'bikini'}},{'confidence' : '7.08091878890991','tag' : {'en': 'summer'}}]}}
-
+    '''
     f=open('wordlist.txt','r')
     wordlist=[]
     for i in f:
